153bd9cd87b17e5b3bbfdcdf838a62995c3aa390
Yeseul Han <yehan@c5r2s1.42seoul.kr>
2022-06-13 08:15:29 +0900

    gnl norm check

--- libft/include/get_next_line.h
+++ libft/include/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: yehan <yehan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/13 07:47:20 by yehan             #+#    #+#             */
-/*   Updated: 2022/06/13 07:47:25 by yehan            ###   ########seoul.kr  */
+/*   Updated: 2022/06/13 07:57:23 by yehan            ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdbool.h>
 # include "../include/libft.h"
 
 # define BUFFER_SIZE 10000
 
 char	*get_next_line(int fd);
-char	*read_iter(char **s_save, int fd);
+char	*read_iter(char *s_save, int fd);
 char	*get_line(char const *s_save);
-char	*set_remains(char **s_save, size_t offset);
+bool	set_remains(char **s_save, size_t offset);
 
 #endif
